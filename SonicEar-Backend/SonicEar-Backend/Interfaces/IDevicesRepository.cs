33bcb06f89fac95cf0887b478ba7509e0ef35387
Nuriye <nuriyeerdogan38@gmail.com>
2024-05-08 18:23:27 +0200

    feat: Interface for DevicesRepository

---
+++ SonicEar-Backend/SonicEar-Backend/Interfaces/IDevicesRepository.cs
@@ -0,0 +1,16 @@
+﻿using SonicEar_Backend.Models;
+
+
+
+namespace SonicEar_Backend.Interfaces
+{
+    public interface IDevicesRepository 
+    {
+        // Interface for DevicesRepository
+        public List<Device> GetAll();
+        public Device GetById (int id);
+        public Device Create(Device device);
+        public Device Update(Device device, int id);
+        public Device Delete(int id);
+    }
+}
