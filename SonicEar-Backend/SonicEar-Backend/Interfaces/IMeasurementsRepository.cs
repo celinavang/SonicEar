442d9cdb350e7596f7493d55821213ce69b1c59c
Pantawan Vongvattanakun (Pall) <pantawan@live.dk>
2024-05-08 16:50:50 +0200

    Update IMeasurementsRepository.cs

--- SonicEar-Backend/SonicEar-Backend/Interfaces/IMeasurementsRepository.cs
+++ SonicEar-Backend/SonicEar-Backend/Interfaces/IMeasurementsRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace SonicEar_Backend.Interfaces
+﻿using SonicEar_Backend.Classes;
+
+namespace SonicEar_Backend.Interfaces
 {
     public interface IMeasurementsRepository
     {
+        public List<Measurement> GetAll();
+        public Measurement? GetById (int id);
+        public List<Measurement> GetByDevice (int deviceId);
+        public List <Measurement> GetByDate (DateTime date);
+        public Measurement Create(Measurement measurement);
+        public Measurement? Update(Measurement measurement, int id);
+        public Measurement? Delete(int id);
+        
+
+
     }
 }
