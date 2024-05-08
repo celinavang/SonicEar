df6b6a381acd3d990dad1246ea8aa77a9fcc6727
akaics <guls0489@edu.zealand.dk>
2024-05-08 17:56:47 +0200

    feat: Device.cs class

---
+++ SonicEar-Backend/SonicEar-Backend/Models/Device.cs
@@ -0,0 +1,21 @@
+﻿namespace SonicEar_Backend.Models
+{
+    public class Device
+    {
+        public int Id { get; set; }
+        public string? Location { get; set; }
+
+        public override string ToString()
+        {
+            return $"Device: {Id}, Location: {Location}";
+        }
+
+        public void VerifyLocation()
+        {
+            if (Location == null)
+            {
+                throw new ArgumentNullException("Location cannot be null");
+            }
+        }
+    }
+}
