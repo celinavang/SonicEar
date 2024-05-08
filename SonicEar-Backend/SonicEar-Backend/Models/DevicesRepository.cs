ae878f3e8a7979fce91335eba1972396b7d5bd81
akaics <guls0489@edu.zealand.dk>
2024-05-08 17:57:11 +0200

    feat: DevicesRepository.cs

---
+++ SonicEar-Backend/SonicEar-Backend/Models/DevicesRepository.cs
@@ -0,0 +1,49 @@
+﻿namespace SonicEar_Backend.Models
+{
+    public class DevicesRepository
+    {
+        
+        private readonly List<Device> _devices;
+
+        public DevicesRepository()
+        {
+            _devices = new List<Device>();
+        }
+
+        public Device Create(Device device)
+        {
+            _devices.Add(device);
+            return device;
+        }
+
+        public List<Device> GetAll()
+        {
+            return new List<Device>(_devices);
+        }
+
+        public Device? GetById(int id)
+        {
+            return _devices.FirstOrDefault(d => d.Id == id);
+        }
+
+        public Device? Update(Device device, int id)
+        {
+            Device? deviceToUpdate = _devices.FirstOrDefault(d => d.Id == id);
+            if (deviceToUpdate != null)
+            {
+                deviceToUpdate.Location = device.Location;
+            }
+            return deviceToUpdate;
+        }
+
+        public Device Delete(int id)
+        { 
+            Device? device = _devices.Find(d => d.Id == id);
+            if (device != null)
+            {
+                _devices.Remove(device);
+            }
+            return device;
+        }
+    }
+}
