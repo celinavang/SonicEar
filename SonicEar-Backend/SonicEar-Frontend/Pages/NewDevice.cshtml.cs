26916e87db259bb51f51bfd3b5163408edcc1aa3
GKierk <45508528+TechnoWeaver@users.noreply.github.com>
2024-05-22 12:47:26 +0200

    Added NewDevice page

---
+++ SonicEar-Backend/SonicEar-Frontend/Pages/NewDevice.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SonicEar_Frontend.Pages
+{
+    public class NewDeviceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
