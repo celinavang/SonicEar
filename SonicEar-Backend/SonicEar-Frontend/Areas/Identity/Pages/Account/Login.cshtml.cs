a9a058a4f2a267df87117e73cd0daf6879685d5f
Pantawan Vongvattanakun (Pall) <pantawan@live.dk>
2024-05-24 17:40:45 +0200

    small fixes

--- SonicEar-Backend/SonicEar-Frontend/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ SonicEar-Backend/SonicEar-Frontend/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -129,7 +129,7 @@ namespace SonicEar_Frontend.Areas.Identity.Pages.Account
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    ModelState.AddModelError(string.Empty, "Ugyldigt login");
                     return Page();
                 }
             }
